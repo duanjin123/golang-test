cd45555bfedbeb016a0cbf4909f4082a269fb321
duanjin <duanjin@dragonest.com>
2022-02-25 18:24:22 +0800

    函数

--- Function/base.go
+++ Function/base.go
@@ -1,1 +1,22 @@
-package Function
+package main
+
+import "fmt"
+
+func main()  {
+	a := 2
+	b := "test"
+	c := map[int]string {
+		0: "aaa",
+		1: "bbb",
+	}
+	d := addMap(a, b, c)
+	fmt.Printf("map c经过函数addMap后的值为：%v\n", d)
+}
+
+/**
+ * 函数声明包括函数名、形式参数列表、返回值列表（可省略）以及函数体。
+ */
+func addMap(a int, b string, c map[int]string) map[int]string {
+	c[a] = b
+	return c
+}
